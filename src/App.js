3e24d3be22fe08aea44f6fea0e3e9cb93f9964d2
Sarah Odiavbara <sarahodiavbara@Sarahs-MBP.lan>
2023-09-02 19:18:22 +0200

    nouveu commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useSelector,useDispatch } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {actionCreators} from './state/index'
 import './App.css';
 
 function App() {
+
+  const account = useSelector((state) => state.account)
+  const dispatch = useDispatch()
+
+  const {depositMoney,withdrawMoney} = bindActionCreators(actionCreators, dispatch)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-3xl'>{account}</h1>
+      <button
+        onClick={() => depositMoney(100)}
+        className="p-3 border rounded-[10px] text-3xl gap-[30px] text-white bg-orange-500"
+      >
+        Deposit
+      </button>
+      <button
+        onClick={() => withdrawMoney(500)}
+        className="p-3 border rounded-[10px] text-3xl gap-[30px] text-white bg-orange-500"
+      >
+        Withdraw
+      </button>
     </div>
   );
 }
---
+++ src/state/store.js
@@ -0,0 +1,8 @@
+// import {createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunk from 'redux-thunk'
+
+export const store = createStore(
+    reducers, {}, applyMiddleware(thunk)
+    )
